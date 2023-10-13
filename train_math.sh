f1dfff51be20b97b5e372690499e91cbb341ea29
CCCarloooo <xd_meng@outlook.com>
2023-10-13 12:21:09 +0800

    Initial commit

---
+++ train_math.sh
@@ -0,0 +1,25 @@
+accelerate launch /home/mengxiangdi/archive/FastChat/fastchat/train/train.py \
+    --model_name_or_path /home/mengxiangdi/models/Llama-2-7b-hf  \
+    --data_path /home/mengxiangdi/archive/math_sys/math_train.json \
+    --fp16 True \
+    --output_dir /home/mengxiangdi/archive/math_sys/math-lm \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 400 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --report_to none \
+    --weight_decay 0 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 10 \
+    --fsdp "full_shard auto_wrap" \
+     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True \
+    --model_max_length 1600 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True 
