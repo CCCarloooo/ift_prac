742c30e604078bdb26b21d56af415eabadf1c8aa
CCCarloooo <xd_meng@outlook.com>
2023-10-24 20:51:45 +0800

    debug ipynb and add some launcher

---
+++ math_sys/sys_ailab_train_math.sh
@@ -0,0 +1,25 @@
+srun -p speech --gres=gpu:8 torchrun --nproc_per_node=8 --master_port=20002 /mnt/petrelfs/libaoxiang/sysprompt/FastChat/fastchat/train/train_mem.py \
+    --model_name_or_path /mnt/petrelfs/libaoxiang/work/Llama-2-7b-hf  \
+    --data_path /mnt/petrelfs/libaoxiang/sysprompt/ift_prac/math_sys/math_train.json \
+    --bf16 True \
+    --output_dir /mnt/petrelfs/libaoxiang/sysprompt/ift_prac/math_sys/math-lm_sys \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "no" \
+    --save_steps 30 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --report_to none \
+    --weight_decay 0 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True \
+    --model_max_length 1600 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True
